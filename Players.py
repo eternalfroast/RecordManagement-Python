a6cbd5f6b3db6c472b800b96bf1881620906f0cb
Saurav Pradhan <42857122+eternalfroast@users.noreply.github.com>
2019-01-02 20:58:53 +1100

    Making final changes
    Making final changes and tweets

--- Players.py
+++ Players.py
@@ -1,6 +1,7 @@
 
 """
 
+
 This is a a simple Soccer Player Management and Visualisation System.
 This system is based on the European Soccer Database on Kaggle,
 which is a platform for predictive modeling and analytics competitions based on the
@@ -57,6 +58,8 @@ def readFile():
         weightList.append(round(float(weight))) #updating the weight list
     playerFile.close()  #closing the file after the completion. It is a must.Otherwise program will not run properly.
 
+
+
 def inputPlayerID(message):
   while True:
     try:
@@ -94,6 +97,130 @@ def inputPlayerAge(message):
        print("Please check the input")
        continue
 
+#this function is used to validate the height of player making sure that it meets certain criteria
+def inputPlayerHeight(message):
+  #this loop runs until the criteria matches
+  while True:
+    try:
+       userInput = input(message)
+       if (userInput.isdigit() and (len(userInput) == 3) and int(userInput)> 100 and int(userInput)< 300):
+           return userInput    
+           break # break is used to break out of loop
+       else:
+           print("Error!! Please enter the valid formatted ID. Player height should be between 100 to 300 . Example: 200")
+           continue
+
+           
+    except ValueError:
+       print("Please check the input")
+       continue
+
+#this function is used to validate the weight of player making sure that it meets certain criteria
+def inputPlayerWeight(message):
+#this loop runs until the criteria matches
+  while True:
+    try:
+       userInput = input(message)
+       if ((len(userInput) == 3 or 4 or 5) and float(userInput)> 50 and float(userInput)< 300):
+           return userInput     
+           break
+       else:
+           print("Error!! Please enter the valid formatted ID. Player weight should be between 50 to 300 . Example: 200.1 or 200")
+           continue  # continue keeps the user in loop 
+
+           
+    except ValueError:
+       print("Please check the input")
+       continue
+
+
+
+
+
+    
+#this function is used to displayed the user entered data to the player
+#this function takes id of player, name of player,age of player, height of player, weight of player as parameter
+def playerEnteredData(playerID, playerName, playerAge, playerHeight,playerWeight):
+    
+    print("The details of the player you entered are as follows: ")
+    print("Player ID: ", playerID)
+    print("Player name: ", playerName)
+    print("Age: ", playerAge)
+    print("Height: ", playerHeight)
+    print("Weight: ", playerWeight)
+
+
+
+
+def addPlayerDetail(playerID, playerName, playerAge, playerHeight,playerWeight):
+    
+    detailAdd = open("Players.txt", 'a')
+    infoToBeWritten = "\n   " + str(playerID) + "\t\t" + '{:>21}'.format(playerName) + "\t\t"+str(playerAge) + "\t" + str(playerHeight) +  "\t    " + str(playerWeight)
+    detailAdd.write (infoToBeWritten)
+    detailAdd.close()
+
+
+
+def searchPlayerDetail(playerID):
+    playerFile = open("Players.txt", 'r')
+    playerFile.readline()
+    playerList= []
+  
+    for line in playerFile:
+        lineList = (line.strip()).split()
+        ID = lineList[0]
+        age = lineList[-3]
+        height = lineList[-2]
+        weight = lineList[-1]
+        name = ''
+        for eachWord in line:
+            if(not eachWord.isdigit() and (eachWord != ".")):
+                 name = name + eachWord
+            name = name.lstrip().rstrip('\n') # this is used to make sure that there is space in the middle of name but not in the front and has no \n
+        playerList = [ID, name, age, height, weight]
+            
+        playerList.append(playerList)
+
+        #when the search criteria matches, it is displayed to the user using playerEnteredData function
+        if (playerID == ID):
+            print (playerEnteredData(ID, name, age, height,weight))
+            
+    playerFile.close() #the file is closed after the intended purpose
+   
+    
+#this function is used to display the chart to the user
+    
+def display(attributeList, attributeName, units):
+
+    import matplotlib.pyplot as plot #importing the matplot library to work with the graph
+
+    #this presents the  title of the graph
+    plot.title("Histogram of " + attributeName)
+    
+    plot.xlabel(attributeName + ' ' + units) #label in the x-axis
+    plot.ylabel("Freqqquecy") #label in the y-axis
+    plot.hist(attributeList, bins = (range(min(attributeList), max(attributeList)+1,10)))  #this is used to modify the graph
+    plot.show() # used for presenting the graph to the user
+
+
+    
+def visualizeData():
+
+    #taking the user input for the selection purpose
+    userInput = input("Please chose one of the following\n1. Age    2. Height    3. Weight")
+    if (userInput == "1"):
+        print("Displaying the ages of all the players")
+        display(ageList, "Ages", "(years)")
+        
+    elif (userInput == "2"):
+        print("Displaying the height of all the players")
+        display(heightList, "Height", "(meter)")
+    elif (userInput == "3"):
+        print("Displaying the weight of all the players")
+        display(weightList, "Weight", "(kilograms)")
+
+    else:
+        print("The option you have selected is not in the option. Please choose again")
 
 #the main function of the program
 def main():
