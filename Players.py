d8286082d3db3c6aa167060a10d00d1515471b49
Saurav Pradhan <42857122+eternalfroast@users.noreply.github.com>
2019-01-02 20:00:30 +1100

    Initial commit
    Just comments

---
+++ Players.py
@@ -0,0 +1,15 @@
+
+"""
+
+This is a a simple Soccer Player Management and Visualisation System.
+This system is based on the European Soccer Database on Kaggle,
+which is a platform for predictive modeling and analytics competitions based on the
+datasets uploaded by companies and users.
+The first line of this file contains the attribute name: "Player ID", "Play Name", "Age", "Height", and "Weight", and each following
+line contains a record for a player.
+
+This system allows a user to add, search and visualise the details of players.
+
+
+"""
+
